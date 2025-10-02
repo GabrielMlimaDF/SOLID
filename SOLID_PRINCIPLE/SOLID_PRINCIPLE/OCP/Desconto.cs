991e32ed42c4dfa739238504da073291df5ed1bb
Gabriel Matos <gabrieldevbrasilia@gmail.com>
2025-10-02 08:28:00 -0300

    first commit

---
+++ SOLID_PRINCIPLE/SOLID_PRINCIPLE/OCP/Desconto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_PRINCIPLE.OCP
+{
+    public abstract class Desconto
+    {
+        public abstract double Calcular(double valor);
+    }
+
+    public class DescontoEletronicos : Desconto
+    {
+        public override double Calcular(double valor) => valor * 0.1;
+    }
+
+    public class DescontoRoupas : Desconto
+    {
+        public override double Calcular(double valor) => valor * 0.2;
+    }
+
+    public class DescontoCalcados : Desconto
+    {
+        public override double Calcular(double valor) => valor * 0.3;
+    }
+}
---
+++ SOLID_PRINCIPLE/SOLID_PRINCIPLE/SRP/GeradorRelatorio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_PRINCIPLE.SRP
+{
+    public class GeradorRelatorio
+    {
+        public string Gerar()
+        {
+            Console.WriteLine("Gerando relatório...");
+            return "Conteúdo exibido!";
+        }
+    }
+
+    public class RelatorioPersistir
+    {
+        public void Salvar(string conteudo)
+        {
+            Console.WriteLine("Salvando relatório...");
+        }
+    }
+}
